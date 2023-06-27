6d2e34d578541de4add89e6c22c2ebafbf9c4eeb
jnnx <78928643+jNNx@users.noreply.github.com>
2023-06-26 21:45:28 -0300

    update
    instalado instituto

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\TipoPersona;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Persona extends Model
 {
     use HasFactory;
+
+    public function tipoPersona()
+    {
+        return $this->hasMany(TipoPersona::class, 'id', 'tipo_persona_id');
+    }
 }
--- app/Models/TipoPersona.php
+++ app/Models/TipoPersona.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class TipoPersona extends Model
 {
     use HasFactory;
+
+    public function Persona()
+    {
+        return $this->belongsTo(Persona::class, 'id', 'persona_id');
+    }
 }
